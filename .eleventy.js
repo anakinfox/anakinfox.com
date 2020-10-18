57cb0e3f1aa57fad400d049b1c15a9b3300eb6d9
Anakin Fox <anakin.fox@outlook.com>
2020-10-18 07:05:44 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -46,8 +46,3 @@ module.exports = function(eleventyConfig) {
         passthroughFileCopy: true
     }
 }
-
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-};
